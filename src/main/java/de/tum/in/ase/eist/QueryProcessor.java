9788b15b77b3b176cefeabb06df81b1d72f76357
Samuel Schott <ge96san@mytum.de>
2022-07-13 11:47:45 +0200

    v1

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,25 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Samuel";
-        } else { // TODO extend the programm here
+        }else if(query.contains("plus")){
+            String number = query.substring(8);
+            int indexPlus = number.indexOf("plus");
+            String number1 = number.substring(0,indexPlus-1);
+            String number2 = number.substring(indexPlus+5);
+            int result = Integer.parseInt(number1)+Integer.parseInt(number2);
+            return String.valueOf(result);
+        }else if(query.contains("largest")){
+            String numbers = query.substring(query.indexOf("largest")+9);
+            String nr[] = numbers.split(",");
+            int max = 0;
+            for(String n: nr){
+                if(Integer.parseInt(n) > max){
+                    max = Integer.parseInt(n);
+                }
+            }
+            return String.valueOf(max);
+        }
+        else { // TODO extend the programm here
             return "";
         }
     }
